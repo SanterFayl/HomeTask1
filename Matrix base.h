aabc507ec5a7ca22ce0ade8b3cf42b6165398711
SanterFayl <130371759+SanterFayl@users.noreply.github.com>
2023-04-10 17:53:52 +0300

    Matrix base.h
    Реализован .h файл

---
+++ Matrix base.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+using namespace std;
+const int N = 18;
+int MatrixGeneration(int(*matrix)[N]);
+int LineSum(int(*matrix)[N]);
+int CheckingForAPositiveLines(int(*matrix)[N]);
+int MatrixSum(int(*matrix)[N]);
