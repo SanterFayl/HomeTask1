59f505938145e3e649e70049a8f66c5a45d9c027
SanterFayl <130371759+SanterFayl@users.noreply.github.com>
2023-04-10 18:07:12 +0300

    Create MatrixSum.cpp
    Добавлена функция, суммирующая элементы матрицы.

---
+++ MatrixSum.cpp
@@ -0,0 +1,17 @@
+#include "Matrix base.h"
+
+int MatrixSum(int(*matrix)[N]) {
+	int sumMatrix = 0;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (i <= j)
+			{
+				sumMatrix += matrix[i][j];
+			}
+		}
+	}
+	cout << "\nSum is " << sumMatrix << '\n';
+	return 0;
+}
